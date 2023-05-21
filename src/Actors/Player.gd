b56add02492088a014ba0ffbea222671efaa8813
Richard <iwannaplaygamesjp@gmail.com>
2023-05-21 17:13:19 -0400

    adding some actor and player code

---
+++ src/Actors/Player.gd
@@ -0,0 +1,9 @@
+extends Actor
+
+export var stomp_impulse: = 600.0
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+	)
